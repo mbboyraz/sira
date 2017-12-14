3ea78d7277f1c85f85bda3f30b52ea8166af429e
Sedat Er <sedater0072@gmail.com>
2017-12-14 21:13:28 +0300

    ...

--- app/src/main/java/com/ecommerce/bitirme/ecommerce/Activity/AdvertActivity.java
+++ app/src/main/java/com/ecommerce/bitirme/ecommerce/Activity/AdvertActivity.java
@@ -7,20 +7,38 @@ import android.widget.Toast;
 import com.ecommerce.bitirme.ecommerce.Activity.Classes.House;
 import com.ecommerce.bitirme.ecommerce.R;
 import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-public class AdvertActivity extends AppCompatActivity {
-    DataSnapshot dataSnapshot;
+public class AdvertActivity extends AppCompatActivity implements ValueEventListener {
 
+    Firebase mRef;
+    Bundle bundle;
+    House house;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advert);
-        Bundle bundle = getIntent().getExtras();
-        House house = new House();
+        bundle = getIntent().getExtras();
 
+        house = new House();
+        mRef = new Firebase("https://ecommerce-1-28620.firebaseio.com/");
+        mRef.addValueEventListener(this);
+
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
         DataSnapshot ucanlar = dataSnapshot.child("ilanlar").child("ev").child(bundle.getString("id"));
 
         ucanlar.getValue(House.class);
         Toast.makeText(this, house.getSehir().toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onCancelled(FirebaseError firebaseError) {
+
     }
 }
